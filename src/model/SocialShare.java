c2a4ef42aed04cb07446d7ae68ee2de60b856a75
dunglt94 <tuandung9594@gmail.com>
2024-11-07 14:36:47 +0700

    first add

---
+++ src/model/SocialShare.java
@@ -0,0 +1,6 @@
+package model;
+
+public interface SocialShare {
+    void setMessage(String message);
+    void share();
+}
---
+++ src/view/Main.java
@@ -0,0 +1,10 @@
+package view;
+
+import model.Client;
+
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.share("This is a post");
+    }
+}
